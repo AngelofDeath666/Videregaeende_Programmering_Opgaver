6f75a49619eb67c146a060f5b46954190439e7f5
Adin1128 <Adin11281128@hotmail.com>
2020-02-03 16:19:19 +0100

    Implemented Vector and Interface

--- src/Week1/Vector/IVector.java
+++ src/Week1/Vector/IVector.java
@@ -1,6 +1,11 @@
 package Vector;
 
 public interface IVector {
-
+    String toString();
+    Vector times(double a);
+    Vector add(Vector a);
+    boolean Equals(Vector a);
+    double getX();
+    double getY();
 
 }
--- src/Week1/Vector/Vector.java
+++ src/Week1/Vector/Vector.java
@@ -1,4 +1,32 @@
 package Vector;
 
-public class Vector {
+public class Vector implements IVector {
+    private double x;
+    private double y;
+
+    public Vector(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString(){
+        String a = "x = "+x+" y = "+y;
+        return a;
+    }
+    public Vector times(double a){
+        return new Vector(x*a,y*a);
+    }
+    public Vector add(Vector a){
+        return new Vector(x+a.getX(),y+a.getY());
+    }
+    public boolean Equals(Vector a){
+        return x==x&&y==y;
+    }
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
+
 }
