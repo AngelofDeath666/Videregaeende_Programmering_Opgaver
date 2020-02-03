f10bac95cd91639932f4424d86be6d08f33c02d2
Adin1128 <Adin11281128@hotmail.com>
2020-02-03 15:49:21 +0100

    Implemented interfaces

--- src/Week1/IComplex.java
+++ src/Week1/IComplex.java
@@ -1,3 +1,22 @@
 public interface IComplex {
-    
+    String toString();
+    double abs();
+    double phase();
+    Complex plus(Complex b);
+    Complex minus(Complex b);
+    Complex times(Complex b);
+    Complex scale(double alpha);
+    Complex conjugate();
+    Complex reciprocal();
+    double re();
+    double im();
+    Complex divides(Complex b);
+    Complex exp();
+    Complex cos();
+    Complex sin();
+    Complex tan();
+    boolean equals(Object x);
+    int hashCode();
+
+
 }
